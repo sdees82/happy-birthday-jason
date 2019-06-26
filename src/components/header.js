7f132f08b4dc5154500d1d6f626204072c58e430
sdees82 <seantedees1982@gmail.com>
2019-06-26 16:55:18 -0400

    changed files staged for commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,8 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
+      display: 'none',
+      title: "Happy birthday Jason!",
       background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
@@ -12,7 +14,6 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import {Random} from 'react-animated-text'
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
-  <Layout>
+  <Layout >
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <img src={Birthday}/> */}
+    <div className="background">
+    <Random
+  text="HAPPY BIRTHDAY JASON!!"
+  effect="jump"
+  effectChange={2.0}
+  effectDuration={0.3}
+/>
+</div>
   </Layout>
 )
 
